85db699370b79bb60940daad3ede08fe4fd98455
dwhit <dwhite005@icloud.com>
2024-08-02 21:13:17 -0400

    Learning Runnable

--- src/Sequential.java
+++ src/Sequential.java
@@ -1,7 +1,19 @@
 public class Sequential {
+    private static void demo1() {
+        for(int i = 0;i < 5;i++) {
+            System.out.println("From demo 1 " + i);
+        }
+    }
+
+    private static void demo2() {
+        for(int i = 0;i < 5;i++) {
+            System.out.println("From demo 2 " + i);
+        }
+    }
     public static void main(String[] args) {
         //Learning how threads work and how to use them in Java
 
-
+        demo1();
+        demo2();
     }
 }
